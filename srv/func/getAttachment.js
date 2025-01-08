b06c00c5f4e8f8ca0ddf07205b678f0ad2838638
Maurizio Moraca <maurizio.moraca@capgemini.com>
2025-01-08 13:44:25 +0000

    Function fixed

--- srv/func/getAttachment.js
+++ srv/func/getAttachment.js
@@ -1,14 +1,18 @@
 const { checkReadScope } = require('./utils/scopes');
+const cds = require('@sap/cds');
 
 "use strict";
 
 module.exports = async (request, tx) => {
-    // Extract PackageId from request query parameters
+    // Extract PackageId and extension from request query parameters
     const packageId = request.req.query.PackageId;
+    let extension = request.req.query.Extension;
+
     if (!packageId) {
         // Return error response if PackageId is not provided
         return { status: 400, message: 'Bad Request' };
     }
+    
 
     const headerFatturaElettronica = (await tx.run(
         SELECT('ID').from('FatturaElettronica').where({ navigation_to_PackageId: packageId })
@@ -16,9 +20,15 @@ module.exports = async (request, tx) => {
     const bodyFatturaElettronica = (await tx.run(
         SELECT('ID').from('FatturaElettronicaBody').where({ header_Id: headerFatturaElettronica.ID })
     ))[0];
-    const allegati = (await tx.run(
+    
+    var allegati = (await tx.run(
         SELECT('*').from('Allegati').where({ body_Id: bodyFatturaElettronica.ID })
-    ))[0];
+    ));
+
+    if (extension) {
+        let upperExtension = extension.toUpperCase();
+        allegati = allegati.filter(allegato => allegato.formatoAttachment.toUpperCase() === upperExtension);
+    }
 
     return { status: 200, result: allegati, message: 'Executed' };
 };
