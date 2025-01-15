0ac482d5b12e989cb7392768986bd40dfa9af649
Maurizio Moraca <maurizio.moraca@capgemini.com>
2025-01-15 14:25:13 +0000

    Added filter to getDeliveryNoteRef function

--- srv/func/getDeliveryNoteRef.js
+++ srv/func/getDeliveryNoteRef.js
@@ -11,6 +11,6 @@ module.exports = async (request, tx) => {
     }
     const serviceS4_HANA = await cds.connect.to(process.env['Destination_OData_S4HANA']);
     const serviceRequestS4_HANA = serviceS4_HANA.tx(request);
-    const oResultDeliveryNoteRefRequest = await serviceRequestS4_HANA.get(process.env['Path_API_YY1_INBOUNDDELIVERIES_CDS']+"&$filter=Supplier eq '"+supplier+"'");
+    const oResultDeliveryNoteRefRequest = await serviceRequestS4_HANA.get(process.env['Path_API_YY1_INBOUNDDELIVERIES_CDS']+"&$filter=Supplier eq '"+supplier+"' and DeliveryDocumentBySupplier ne ''");
     return { status: 200, result: oResultDeliveryNoteRefRequest, message: 'Executed' };
 };
